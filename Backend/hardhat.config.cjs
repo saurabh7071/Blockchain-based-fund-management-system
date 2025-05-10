5d7fceffbddf1361d7159b5f513462b81a1f75ca
No-One-code-stack <everyonecannoticeme@gmail.com>
2025-05-11 03:34:29 +0530

    deployed them on polygon scan

--- Backend/hardhat.config.cjs
+++ Backend/hardhat.config.cjs
@@ -9,4 +9,9 @@ module.exports = {
       accounts: [process.env.SUPERADMIN_PRIVATE_KEY],
     },
   },
+  etherscan: {
+    apiKey: {
+      polygonAmoy: "XBEJ8XINF9NG4P4JY2Z82CCE26TEXSD93G"  // Replace with your actual API key from Polygonscan
+    }
+  }
 };
